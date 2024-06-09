720f29cf042c3e7b8ba1f2ad81e5e3aee1980343
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-08 21:33:09 -0400

    Netlify link added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# [Netlify Live Link](Netlify link here)
+# [Netlify Live Link](https://666505e8254c8b604ac04279--chipper-madeleine-d395cc.netlify.app/)
 # [GitHub Pages Live Link](https://kcap923.github.io/react-fashion-blog/)
 # [GitHub Link of HTML Version](https://github.com/KCap923/react-fashion-blog)
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,108 @@
-import Nav from './components/Nav';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Article from './components/Article';
+// import Nav from './components/Nav';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import Article from './components/Article';
 // import App from './components/App';
 import './App.css';
 
 
 function App() {
+
+const title = "This page is under construction";
+
   
   return ( 
-<>
+  <>
+<div className='App'>
+  <div className='content'>
+    <h1> {title} </h1>
+  </div>
 
-<Nav/>
-<Header/>
-<Footer/>
-<Article/>
+</div>
+
+
+{/* 
 <App/>
 
 
+<Header/>
+<header>
+<h1> Sartres's List </h1>
+<h2> Better-Dressed People </h2>
+</header>
+
+
+<Nav/>
+<nav aria-label="Main Navigation" role="navigation">
+  <ul class="nav-bar">
+    <li><a href="..."> Women's </a></li>
+    <li><a href="..."> Men's </a> </li>
+    <li><a href="..."> On The Street </a></li>
+    <li><a href="..."> The Catwalk </a></li>
+    <li><a href="..."> AdWatch </a></li>
+    <li><a href="..."> About </a></li>
+      </ul>
+</nav>
+<br></br>
+
+
+<Article/>
+
+<main>  </main>
+<section>
+  <article>
+    <time datetime="2020-11-12">11/12/20</time>
+      <h4> On The Street In Brooklyn </h4>
+
+<img src="images/blog-image-1.jpg" alt="Woman on streets of Brooklyn" srcset="">
+<p> 
+      Consectetur quasi minus quibusdam magni nam vitae libero accusantium sed fugiat maxime facilis natus, adipisci nisi dolorem modi temporibus commodi harum hic rerum placeat aliquam deleniti quis repellendus! Harum ipsum a deserunt, sunt officiis non assumenda illo quis vel iste maiores laborum incidunt quae. Molestiae, ex labore assumenda veritatis distinctio nobis illo sapiente minima soluta eligendi quidem corrupti vitae cupiditate dolore, reprehenderit debitis odio natus sint similique mollitia? Quae fugiat quos nulla natus deserunt dicta harum recusandae explicabo aperiam quo perspiciatis provident magni nemo iure, hic cumque similique ipsam voluptatibus qui sed dolorem earum autem! Non rerum qui sapiente unde amet ipsa cum voluptatibus beatae in illum, nisi ad totam officiis sed laudantium iste. Quia totam nisi harum quidem deserunt repudiandae cupiditate distinctio eaque fuga facilis. Delectus at corrupti quidem ipsam, blanditiis rem vero maxime aut minus, esse provident nobis illum dolor. Saepe facilis molestias aut praesentium fugit labore neque dolores, perspiciatis ducimus. Nostrum nisi ea doloremque eaque accusantium provident earum officia sit sint doloribus ex temporibus officiis vel tempora fugit nemo non rerum magnam nam velit repellat architecto, voluptates, tenetur in. Rem molestiae dolores molestias voluptatum soluta at vitae quasi aperiam quibusdam magnam, ex accusantium sed aspernatur numquam praesentium, doloremque autem dolorum maiores.Ea pariatur laudantium natus quaerat molestias enim ex fugit optio tenetur inventore doloribus quasi beatae, consectetur aliquam eveniet iusto tempora repellat quae fuga, dolor ipsa iste maxime dignissimos? Minus ea blanditiis, sunt nisi magnam neque harum odit veniam. Aut maxime deleniti velit, illo repellat amet.
+    </p>
+</article>
+</section>
+<h3> Continues ... </h3> 
+<br>
+<br>
+<hr>
+
+<!-- Second Post -->
+<br>
+<section>
+  <article>
+    <time datetime="2020-11-11"> 11/11/20 </time>
+    <h4> Vintage in Vogue </h4>
+
+<img src="images/blog-image-2.jpg" alt="Models in vintage clothing neck up" srcset="">
+
+    <p>
+    Sapiente odit minus aut rem eius voluptatem? Et, quibusdam quisquam? Doloribus ullam eligendi vitae officiis itaque nesciunt sint omnis reiciendis, sunt nostrum! Minima quos dolor veritatis eligendi quisquam hic molestiae a repellendus expedita doloremque sint nihil necessitatibus amet fuga consequatur earum, debitis quis ut tempora? Possimus omnis error dolor delectus dolore maiores molestias ducimus itaque, fugiat voluptas temporibus nostrum vel placeat adipisci quaerat necessitatibus inventore nobis alias rerum ipsum fugit pariatur eius? Animi delectus a sint est adipisci suscipit at ipsum fuga quibusdam? Deleniti possimus fugiat mollitia earum distinctio nisi dolor porro, doloribus nam est itaque ullam repellat nesciunt eum perferendis. Perferendis labore obcaecati ad fuga qui neque, illo iure esse iste quia, distinctio odio perspiciatis delectus fugiat autem nam aut. Nihil, fuga. Culpa, ducimus consectetur delectus at eveniet eaque recusandae a est fugit repudiandae quam minima molestiae adipisci repellat neque? Numquam incidunt sequi eveniet ipsam vero repudiandae, neque ipsa magnam doloremque ex modi aliquam, tempore, sed voluptatibus alias voluptatem. Impedit, sit atque? Dolorum distinctio commodi officia quam laudantium, eos praesentium natus sint perferendis! Quo necessitatibus vel, tempora illo pariatur veritatis a vitae harum impedit voluptatem minus perspiciatis nihil rerum dicta at consequatur libero soluta, animi ullam sunt quaerat! 
+    </p>
+    <aside>
+    
+    </aside>
+  </article>
+</section>
+<h3> Continues ... </h3>
+<br>
+<hr></hr> 
+
+
+<Footer/>
+<br>
+<br>
+<nav aria-label="Main Navigation" role="navigation" >
+  <ul class="footer-bar">
+    <li><a href="..." style="color: tomato;"> Women's </a></li>
+    <li><a href="..." style="color: tomato;"> Men's </a> </li>
+    <li><a href="..." style="color: tomato;"> On The Street </a></li>
+    <li><a href="..." style="color: tomato;"> The Catwalk </a></li>
+    <li><a href="..." style="color: tomato;"> AdWatch </a></li>
+    <li><a href="..." style="color: tomato;"> About </a></li>
+      </ul>
+</nav>
+
+<footer> &copy; 2013 Valet Industries, Inc </footer>
+<br></br> */}
 
 </>
   )
